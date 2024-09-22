56d9f83e69d2dbe170be10498a7a7702870e0f7e
ikumisato <kinoko7971@gmail.com>
2024-09-23 06:41:42 +0900

    リンクを修正

--- header.php
+++ header.php
@@ -13,12 +13,10 @@
     <link href="<?php echo get_template_directory_uri(); ?>/css/reset.css" media="all" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/slick/slick.css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/slick/slick-theme.css" />
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" media="all" type="text/css" />
 
     <script src="<?php echo get_template_directory_uri(); ?>https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/slick/slick.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/main.js"></script>
     <?php wp_head();?>
   </head>
   <body>
@@ -29,12 +27,12 @@
       </div>
       <!-- PC用ナビゲーション -->
       <nav id="nav-pc">
-        <a href="<?php echo home_url(); ?>/about">About</a>
-        <a href="<?php echo home_url(); ?>/product">product</a>
-        <a href="<?php echo home_url(); ?>/news">News</a>
-        <a href="<?php echo home_url(); ?>/shop">Shopping</a>
-        <a href="<?php echo home_url(); ?>/contact">Contact</a>
-        <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/image/PC/cart.png" class="online-btn" /></a>
+        <a href="#about">About</a>
+        <a href="#product">product</a>
+        <a href="#news">News</a>
+        <a href="<?php echo get_template_directory_uri(); ?>/shop.php">Shopping</a>
+        <a href="#contact">Contact</a>
+        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/PC/cart.png" class="online-btn" /></a>
       </nav>
     </header>
     <!-- /header -->
